fdf1f103ca82f1b566d92bac5e77724f8b896be5
Lauro Soares <lauro_bs8@hotmail.com>
2019-10-03 09:51:53 -0300

    primeiro-componente/armazenar-input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import logo from './assets/logo.svg'
+import api from './services/api'
 
 function App() {
+
+  const [email, setEmail] = useState('') // toda vez que esse campo for alterado, a variavel email ira atualizar, setEmail -> atualizar o valor do email
+
+  async function handleSubmit(event) {
+    event.preventDefault(); // previnir do efeito padrao de submit, a troca de tela
+
+    // console.log(email)
+
+    const response = await api.post('/sessions', { email })
+  
+    const {_id} = response.data
+
+    localStorage.setItem('user', _id)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={logo} alt="Aircnc" />
+
+      <div className="content">
+        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL *</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Seu melhor email"
+            value={email}
+            onChange={event => setEmail(event.target.value)} />
+
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+
+      </div>
     </div>
   );
 }
