176f8a7f30eca50a5f907806ff793b7d370fc7d9
Lauro Soares <lauro_bs8@hotmail.com>
2019-10-14 15:07:37 -0300

    Avançado-socket no backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,11 +13,6 @@ const app = express()
 const server = http.Server(app) // extraindo o servidor http do express
 const io = socketio(server) // agora o servidor esta reconhecendo o websocket
 
-io.on('connection', socket => {
-  console.log('Usuario conectado', socket.id) // toda comunicação em tempo real que se conecta com o backend precisara de um id unico
-})
-
-
 mongoose.connect('mongodb+srv://omnistack:omnistacksenha@omnistack-aircnc-cxfji.mongodb.net/semana09', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -26,6 +21,24 @@ mongoose.connect('mongodb+srv://omnistack:omnistacksenha@omnistack-aircnc-cxfji.
 // req.query = acessar os params (para filtros)
 // req.params = acessar router params (para edição e delete)
 // req.body = acessar corpo da requisição (para criação, edição) Nao reconhece formato json, necessario colocar app.use(express.json()) pra ser reconhecido
+
+const connectedUsers = {} // não é o melhor jeito de armazenar num aplicativo em produção, em ambientes de produção indicado o uso do Reddis
+
+io.on('connection', socket => {
+  // console.log('Usuario conectado', socket.id) // toda comunicação em tempo real que se conecta com o backend precisara de um id unico
+  //console.log(socket.handshake.query)
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+
+})
+
+app.use((req, res, next) => {
+  req.io = io // aqui faz com que todas as rotas da aplicação tenha acesso ao io, que permite enviar e receber do front ou do mobile
+  req.connectedUsers = connectedUsers // acesso aos usuarios acessados na aplicação
+
+  return next() // pra seguir o fluxo normal da aplicação. Sem isso, a aplicação fica presa nesse bloco
+})
 
 
 app.use(cors()) // qualquer tipo de aplicação possa acessar a api
